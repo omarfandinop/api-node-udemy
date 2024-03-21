a7c765d508d2aa58995688e344c53d6a6bf3b33d
Omar Fandiño <omarfandinop@gmail.com>
2024-03-21 12:32:18 -0500

    Rest API (WIP)

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
